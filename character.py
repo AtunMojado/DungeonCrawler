15aced5ceb46b0e5c151aad40a0d40ca1d6391d2
Alberto <albertorondang@gmail.com>
2023-04-29 12:44:33 +0200

    `Last update: add movement to the player´

--- character.py
+++ character.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 import constants
 
@@ -6,5 +8,17 @@ class Character():
         self.rect = pygame.Rect(0, 0, 40, 40)
         self.rect.center = (x, y)
 
+    def move(self, dx, dy):
+
+        #control diagonal speed
+        if dx != 0 and dy != 0: #if one coord is 0, movement is horizontal or vertical
+            dx = dx * (math.sqrt(2)/2)
+            dy = dy * (math.sqrt(2)/2)
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+
     def draw(self, surface):
         pygame.draw.rect(surface, (constants.RED), self.rect)
+
--- main.py
+++ main.py
@@ -9,12 +9,41 @@ screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGH
 #window title
 pygame.display.set_caption('Dungeon Crawler')
 
+#create clock for maintaning frame rate
+clock = pygame.time.Clock()
+
 #create player
 player = Character(100, 100)
+
+#define player movement variables
+moving_left = False
+moving_right = False
+moving_up = False
+moving_down = False
 
 #main game loop
 run = True
 while run:
+
+    #control frame rate
+    clock.tick(constants.FPS)
+
+    screen.fill(constants.BG)
+
+    #calculate player movement(change in x and y)
+    dx = 0
+    dy = 0
+    if moving_right == True:
+        dx = constants.SPEED
+    if moving_left == True:
+        dx = -constants.SPEED
+    if moving_up == True:
+        dy = -constants.SPEED
+    if moving_down == True:
+        dy = constants.SPEED
+
+    #move player
+    player.move(dx, dy)
 
     #draw player on screen
     player.draw(screen)
@@ -23,6 +52,28 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        #take keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_w:
+                moving_up = True
+            if event.key == pygame.K_s:
+                moving_down = True
+
+        #keyboard button released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_w:
+                moving_up = False
+            if event.key == pygame.K_s:
+                moving_down = False
+
 
     pygame.display.update()
 
