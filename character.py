f403c8ef4cde7905c064d0f96094f8db13eaf32a
Alberto <albertorondang@gmail.com>
2023-04-29 13:47:24 +0200

    `Last update: set animation and flip to the moving player´

--- character.py
+++ character.py
@@ -4,11 +4,21 @@ import pygame
 import constants
 
 class Character():
-    def __init__(self, x, y):
+    def __init__(self, x, y, animation_list):
+        self.flip = False
+        self.animation_list = animation_list
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        self.image = animation_list[self.frame_index]
         self.rect = pygame.Rect(0, 0, 40, 40)
         self.rect.center = (x, y)
 
     def move(self, dx, dy):
+
+        if dx < 0: #player moves to the left
+            self.flip = True #player flips to the left side
+        if dx > 0:
+            self.flip = False
 
         #control diagonal speed
         if dx != 0 and dy != 0: #if one coord is 0, movement is horizontal or vertical
@@ -18,7 +28,22 @@ class Character():
         self.rect.x += dx
         self.rect.y += dy
 
+    def update(self):
+        #handle animation and update image
+        animation_cooldown = 70
+        self.image = self.animation_list[self.frame_index]
+        #checking if enough time has passed since last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        #checking if animation range has finished, if not it gives an error
+        if self.frame_index >= len(self.animation_list):
+            self.frame_index = 0
+
+
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (constants.RED), self.rect)
+        flipped_image = pygame.transform.flip(self.image, self.flip, False)
+        surface.blit(flipped_image, self.rect)
+        pygame.draw.rect(surface, (constants.RED), self.rect, 1)
 
--- main.py
+++ main.py
@@ -12,16 +12,29 @@ pygame.display.set_caption('Dungeon Crawler')
 #create clock for maintaning frame rate
 clock = pygame.time.Clock()
 
-#create player
-player = Character(100, 100)
-
 #define player movement variables
 moving_left = False
 moving_right = False
 moving_up = False
 moving_down = False
 
-#main game loop
+#helper function to scale image
+def scale_img(image, scale):
+    w = image.get_width()
+    h = image.get_height()
+    return pygame.transform.scale(image, (w * scale, h * scale))
+
+animation_list = []
+for i in range(4):
+
+    img = pygame.image.load(f'assets/images/characters/elf/idle/{i}.png').convert_alpha()
+    img = scale_img(img, constants.SCALE)
+    animation_list.append(img)
+
+#create player
+player = Character(100, 100, animation_list)
+
+#MAIN GAME LOOP!
 run = True
 while run:
 
@@ -44,6 +57,9 @@ while run:
 
     #move player
     player.move(dx, dy)
+
+    #update player
+    player.update()
 
     #draw player on screen
     player.draw(screen)
