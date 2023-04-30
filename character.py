d7e6217c3c24a2c012e1cbd46fd9bc639853894d
Alberto <albertorondang@gmail.com>
2023-04-30 12:38:19 +0200

    `Last update: set different moves (idle and run) for the character and load different images for the character´

--- character.py
+++ character.py
@@ -4,17 +4,23 @@ import pygame
 import constants
 
 class Character():
-    def __init__(self, x, y, animation_list):
+    def __init__(self, x, y, mob_animations, char_type):
+        self.char_type = char_type
         self.flip = False
-        self.animation_list = animation_list
+        self.animation_list = mob_animations[char_type]
         self.frame_index = 0
+        self.action = 0 # 0 is IDLE, 1 is RUNNING
         self.update_time = pygame.time.get_ticks()
-        self.image = animation_list[self.frame_index]
+        self.running = True
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = pygame.Rect(0, 0, 40, 40)
         self.rect.center = (x, y)
 
     def move(self, dx, dy):
+        self.running = False
 
+        if dx != 0 or dy != 0: #if this coords are not 0, there is movement
+            self.running = True
         if dx < 0: #player moves to the left
             self.flip = True #player flips to the left side
         if dx > 0:
@@ -28,20 +34,34 @@ class Character():
         self.rect.x += dx
         self.rect.y += dy
 
-    def update(self): #updating the state of the moving and flipped image
+    # updating the state of the moving and flipped image
+    def update(self):
+        #check what action the player is performing
+        if self.running == True:
+            self.update_action(1)# 1 = RUN
+        else:
+            self.update_action(0)# 0 = IDLE
+
+
         #handle animation and update image
         animation_cooldown = 75
-        self.image = self.animation_list[self.frame_index]
+        self.image = self.animation_list[self.action][self.frame_index]
         #checking if enough time has passed since last update
         if pygame.time.get_ticks() - self.update_time > animation_cooldown:
             self.frame_index += 1
             self.update_time = pygame.time.get_ticks()
         #checking if animation range has finished, if not it gives an error
-        if self.frame_index >= len(self.animation_list):
+        if self.frame_index >= len(self.animation_list[self.action]):
             self.frame_index = 0
 
-
-
+    #helper function to define if character is IDLE or RUNNING
+    def update_action(self, new_action):
+        #check if new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            #update animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
     def draw(self, surface):
         flipped_image = pygame.transform.flip(self.image, self.flip, False)
         surface.blit(flipped_image, self.rect)
--- main.py
+++ main.py
@@ -24,14 +24,26 @@ def scale_img(image, scale):
     h = image.get_height()
     return pygame.transform.scale(image, (w * scale, h * scale))
 
-animation_list = []
-for i in range(4):
-    img = pygame.image.load(f'assets/images/characters/elf/idle/{i}.png').convert_alpha()
-    img = scale_img(img, constants.SCALE)
-    animation_list.append(img)
+#load character images
+mob_animations = []
+mob_types = ['elf', 'imp', 'skeleton', 'goblin', 'muddy', 'tiny_zombie', 'big_demon']
+animation_types = ['idle', 'run']
+for mob in mob_types:
+    #load images
+    animation_list = []
+    for animation in animation_types:
+        #reset temporary list of images
+        temp_list = []
+        for i in range(4):
+            img = pygame.image.load(f'assets/images/characters/{mob}/{animation}/{i}.png').convert_alpha()
+            img = scale_img(img, constants.SCALE)
+            temp_list.append(img)
+        animation_list.append(temp_list)
+    mob_animations.append(animation_list)
+
 
 #create player
-player = Character(100, 100, animation_list)
+player = Character(100, 100, mob_animations, 1)
 
 #MAIN GAME LOOP!
 run = True
