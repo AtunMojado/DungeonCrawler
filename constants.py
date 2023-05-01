c42f39f1d1888c6c2e68dbb55283645fe1820005
Alberto <albertorondang@gmail.com>
2023-05-02 00:03:54 +0200

    `Last update: set character life hearts and their draw in screen´

--- constants.py
+++ constants.py
@@ -4,11 +4,14 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCALE = 3
 WEAPON_SCALE = 1.5
+ITEM_SCALE = 3
 SPEED = 5
 ARROW_SPEED = 10
 OFFSET = 12 #space between rect created and character
 
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 #background color
 BG = (40, 25, 25)
+PANEL = (50, 50, 50)
